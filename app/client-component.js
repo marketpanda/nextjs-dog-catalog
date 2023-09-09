888ab173aaf335a715c2a53d35c3b874e0f43c69
marketpanda <91251429+marketpanda@users.noreply.github.com>
2023-09-09 13:04:05 +0700

    corrected maps in comments

--- app/client-component.js
+++ app/client-component.js
@@ -100,10 +100,10 @@ export default function ClientComponent({
 
   }
 
-  const newArr = Object.values(parsedComments)
+  const commentsArr = Object.values(parsedComments)
 
   {
-    console.log('way to go \n', newArr)
+    console.log('way to go \n', commentsArr)
      
   } 
   return (
@@ -148,20 +148,13 @@ export default function ClientComponent({
             </svg>
             &nbsp;Like
           </button>
-          {/* <button onClick={liked} className="m-2 ml-0 mr-1   bg-red-500 hover:bg-red-700 text-sm text-white py-2 px-4 rounded inline-flex items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z" />
-            </svg>
-
-            &nbsp;Share
-            
-          </button> */}
+         
           <FacebookShareButton><FacebookIcon size={36} borderRadius={8} /></FacebookShareButton>
         </div> 
  
       <div>
         <div>
-          {newArr ? (newArr.map((comment, i) => (
+          {commentsArr ? (commentsArr.map((comment, i) => (
             <>
                
                 <div class="flex flex-col space-y-4 my-1">
