85101f403fc25afb45ed83ac627e0b6a85a1d7e1
marketpanda <91251429+marketpanda@users.noreply.github.com>
2023-09-10 12:23:20 +0700

    removes caches on middleware

--- next.config.js
+++ next.config.js
@@ -3,9 +3,7 @@ const nextConfig = {
     images: {
         domains: ['images.dog.ceo', 'clipart-library.com', 'res.cloudinary.com'] 
     }
-    
-        // loader: 'cloudinary',
-        // path: 'https://res.cloudinary.com/dd7ticf6t'
+     
 }
 
 module.exports = nextConfig
