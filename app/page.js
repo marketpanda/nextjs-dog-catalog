254d95b903ce47d0a979012759c8183ca7233cde
marketpanda <91251429+marketpanda@users.noreply.github.com>
2023-09-11 15:53:15 +0700

    removed TOML

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import ClientComponent from './client-component'
  
 
-export const dynamic = 'force-dynamic'
+// export const dynamic = 'force-dynamic'
 export default async function Home() {
 
   async function getData() {
